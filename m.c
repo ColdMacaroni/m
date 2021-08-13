0bcad8f297925b92d501a0b9e71c72b757d2a3e5
Boat <57285804+ColdMacaroni@users.noreply.github.com>
2021-08-13 15:05:59 +1200

    mmmmmm

---
+++ m.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int
+main()
+{
+    char string[BUFSIZ];
+    char ch;
+
+    while ((ch = getc(stdin)) != EOF)
+    {
+        ungetc(ch, stdin);
+        fgets(string, BUFSIZ, stdin);
+        for(int i = 0; string[i] != '\0'; i++)
+        {
+            if ('a' <= string[i] && string[i] <= 'z')
+                putchar('m');
+            else if ('A' <= string[i] && string[i] <= 'Z')
+                putchar('M');
+            else
+                putchar(string[i]);
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
+}
